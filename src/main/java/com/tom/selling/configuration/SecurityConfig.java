ff8932aa796e9bb14ad7a01fd4234fbf957f56c6
Tom Manger <tom.manger@vaudience.net>
2022-06-27 21:17:11 +0200

    Testing

--- src/main/java/com/tom/selling/configuration/SecurityConfig.java
+++ src/main/java/com/tom/selling/configuration/SecurityConfig.java
@@ -9,10 +9,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.
-
                 authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
                 .and()
                 .formLogin();
     }
