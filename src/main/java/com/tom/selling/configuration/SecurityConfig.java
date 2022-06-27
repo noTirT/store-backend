bcb08bf7ee372a4365393b7801b1cc6aee7513b4
Tom Manger <tom.manger@vaudience.net>
2022-06-27 21:11:48 +0200

    Testing

--- src/main/java/com/tom/selling/configuration/SecurityConfig.java
+++ src/main/java/com/tom/selling/configuration/SecurityConfig.java
@@ -9,8 +9,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.
-                csrf().disable()
-                .authorizeRequests()
+
+                authorizeRequests()
                 .antMatchers("/art/**", "/categories/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
