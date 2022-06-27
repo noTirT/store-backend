ee2199eb8595f6569134f71f850ec7363cf48b63
Tom Manger <tom.manger@vaudience.net>
2022-06-28 00:38:18 +0200

    Testing

--- src/main/java/com/tom/selling/configuration/SecurityConfig.java
+++ src/main/java/com/tom/selling/configuration/SecurityConfig.java
@@ -15,7 +15,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .httpBasic().and()
                 .authorizeRequests()
-                .antMatchers("/art/**", "/categories/**").hasRole("USER")
+                .antMatchers("/art/**", "/categories/**").anonymous()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .and()
                 .csrf().disable();
