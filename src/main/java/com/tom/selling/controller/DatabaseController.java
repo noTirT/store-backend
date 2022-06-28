5b616a58f5e2cf0ab7ba0ae8703f01aca1b22a66
Tom Manger <tom.manger@vaudience.net>
2022-06-28 14:25:58 +0200

    Removed file upload (handled in frontend)

--- src/main/java/com/tom/selling/controller/DatabaseController.java
+++ src/main/java/com/tom/selling/controller/DatabaseController.java
@@ -1,11 +1,9 @@
 package com.tom.selling.controller;
 
 import com.tom.selling.model.ArtPiece;
-import com.tom.selling.model.ArtPieceUpload;
 import com.tom.selling.model.Category;
 import com.tom.selling.service.DataService;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,10 +12,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Iterator;
 import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -39,7 +35,7 @@ public class DatabaseController {
     }
 
     @PostMapping("/art")
-    public ResponseEntity<Void> createNew(@RequestBody ArtPieceUpload artPiece) {
+    public ResponseEntity<Void> createNew(@RequestBody ArtPiece artPiece) {
         this.dataService.createNew(artPiece);
         return new ResponseEntity<>(HttpStatus.OK);
     }
--- src/main/java/com/tom/selling/service/DataService.java
+++ src/main/java/com/tom/selling/service/DataService.java
@@ -1,23 +1,15 @@
 package com.tom.selling.service;
 
-import com.tom.selling.controller.ImageClient;
 import com.tom.selling.model.ArtPiece;
-import com.tom.selling.model.ArtPieceUpload;
 import com.tom.selling.model.Category;
-import com.tom.selling.model.ImageUploadClientResponse;
 import com.tom.selling.repository.ArtRepository;
 import com.tom.selling.repository.CategoryRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import javax.sql.DataSource;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
@@ -31,12 +23,6 @@ public class DataService {
 
     @Autowired
     private CategoryRepository categoryRepository;
-
-    @Autowired
-    private ImageClient imageClient;
-
-    @Value("${api.key}")
-    private String apiKey;
 
     public List<ArtPiece> getAll() {
         return StreamSupport
@@ -54,12 +40,11 @@ public class DataService {
         return artRepository.findById(id);
     }
 
-    public void createNew(ArtPieceUpload artPiece) {
-        ImageUploadClientResponse response = imageClient.uploadImage(apiKey, artPiece.getBase64Image());
+    public void createNew(ArtPiece artPiece) {
         if(!getCategoryNames(getAllCategories()).contains(artPiece.getCategory())){
             categoryRepository.save(new Category(artPiece.getCategory()));
         }
-        artRepository.save(ArtPiece.of(artPiece, response.getData().get("url").toString()));
+        artRepository.save(artPiece);
     }
 
     private List<String> getCategoryNames(List<Category> categories){
