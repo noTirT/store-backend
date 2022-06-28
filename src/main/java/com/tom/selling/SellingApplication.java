0696d7cccc8e05773284fcd73cef834cc88e7313
Tom Manger <tom.manger@vaudience.net>
2022-06-28 13:38:54 +0200

    Fixed dependency issues

--- src/main/java/com/tom/selling/SellingApplication.java
+++ src/main/java/com/tom/selling/SellingApplication.java
@@ -1,18 +1,14 @@
 package com.tom.selling;
 
-import com.tom.selling.model.ArtPiece;
-import com.tom.selling.repository.ArtRepository;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
 public class SellingApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SellingApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SellingApplication.class, args);
+    }
 }
