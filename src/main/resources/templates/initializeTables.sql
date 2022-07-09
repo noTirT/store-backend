7aad8bf74293966de0844c83487269693a76c68d
Tom Manger <tom.manger@vaudience.net>
2022-07-10 01:04:42 +0200

    Fixed bugs in initializeTables.sql Updated DatabaseModel.png Added .drawio file for DatabaseModel.png

--- src/main/resources/templates/initializeTables.sql
+++ src/main/resources/templates/initializeTables.sql
@@ -1,6 +1,7 @@
 CREATE TABLE category (
-category_id int PRIMARY KEY NOT NULL,
-category_name text NOT NULL
+category_id int NOT NULL,
+category_name text NOT NULL,
+PRIMARY KEY(category_id)
 );
 
 CREATE TABLE artpiece (
@@ -9,7 +10,7 @@ name text NOT NULL,
 price REAL NOT NULL,
 description text NOT NULL,
 category_id int NOT NULL,
-PRIMARY KEY(artpiece_id)
+PRIMARY KEY(artpiece_id),
 CONSTRAINT fk_category
 FOREIGN KEY(category_id)
 REFERENCES category(category_id)
@@ -18,10 +19,14 @@ REFERENCES category(category_id)
 CREATE TABLE image (
 image_id int NOT NULL,
 link text NOT NULL,
-PRIMARY KEY(image_id)
+artpiece_id int NOT NULL,
+PRIMARY KEY(image_id),
+CONSTRAINT fk_artpiece
+FOREIGN KEY(artpiece_id)
+REFERENCES artpiece(artpiece_id)
 );
 
-CREATE TABLE order (
+CREATE TABLE artorder (
 order_id int NOT NULL,
 total REAL NOT NULL,
 customer_email text NOT NULL,
@@ -34,15 +39,16 @@ CREATE TABLE orderitem (
 orderitem_id int NOT NULL,
 amount INT NOT NULL,
 order_id INT NOT NULL,
+PRIMARY KEY(orderitem_id),
 CONSTRAINT fk_order
 FOREIGN KEY(order_id)
-REFERENCES order(order_id)
+REFERENCES artorder(order_id)
 );
 
 CREATE TABLE orderartpiece (
 orderitem_id int NOT NULL,
 artpiece_id int NOT NULL,
-PRIMARY KEY (orderitem_id, artpiece_id)
-CONSTRAINT fk_orderitem FOREIGN KEY(orderitem_id) REFERENCES orderitem(orderitem_id)
+PRIMARY KEY (orderitem_id, artpiece_id),
+CONSTRAINT fk_orderitem FOREIGN KEY(orderitem_id) REFERENCES orderitem(orderitem_id),
 CONSTRAINT fk_artpiece FOREIGN KEY(artpiece_id) REFERENCES artpiece(artpiece_id)
 );
