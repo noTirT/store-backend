5752197401f4829443ceb75cb6ba1559a11d3484
Tom Manger <tom.manger@vaudience.net>
2022-07-20 17:07:19 +0200

    A lot of changes

--- src/main/java/com/tom/selling/SellingApplication.java
+++ src/main/java/com/tom/selling/SellingApplication.java
@@ -1,12 +1,25 @@
 package com.tom.selling;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SellingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SellingApplication.class, args);
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Store application")
+                        .version("Some version")
+                        .description("Backend for a store application")
+                );
     }
 }
